ff05bbccebab1f1c87533493ee994e180bb1b4e1
Your NameKariba <you@example.comkaribayasmin@gmail.com>
2020-03-02 10:50:03 +0600

    This App I have developed for learning and developing my skill purpose in my University Life

---
+++ app/src/main/java/com/example/karibayasmin/volumecontrol/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.karibayasmin.volumecontrol;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar alarm, mediaPlayer, ringer, notification;
+    AudioManager audioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alarm = (SeekBar)findViewById(R.id.seekBar1);
+        mediaPlayer = (SeekBar)findViewById(R.id.seekBar2);
+        ringer = (SeekBar)findViewById(R.id.seekBar3);
+        notification = (SeekBar)findViewById(R.id.seekBar4);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        alarm.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM));
+
+        mediaPlayer.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+
+        ringer.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_RING));
+
+        notification.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION));
+
+
+
+        alarm.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume(AudioManager.STREAM_ALARM, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        mediaPlayer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        ringer.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume(AudioManager.STREAM_RING, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        notification.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
